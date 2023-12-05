3083156f02098c48d064116239a62381fafee8f0
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-12-05 01:47:07 +0000

    add: signUp form

---
+++ crates/ui-components/src/layout.rs
@@ -0,0 +1,36 @@
+#![allow(non_snake_case)]
+
+use  dioxus::prelude::*;
+
+#[derive(Props)]
+pub struct AppLayoutProps<'a> {
+    title: &'a str,
+    children: Element<'a>,
+}
+
+pub fn Layout<'a>(cx: Scope<'a, AppLayoutProps<'a>>) -> Element {
+    cx.render(rsx!(
+        {
+            LazyNodes::new(|f| f.text(format_args!("<!DOCTYPE html><html lang='en'>")))
+        }
+        head {
+            title {
+                "{cx.props.title}"
+            }
+            meta {
+                charset: "utf-8"
+            }
+            meta {
+                "http-equiv": "X-UA-Compatible",
+                content: "IE=edge"
+            }
+            meta {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
+            }
+        }
+        body {
+            &cx.props.children
+        }
+    ))
+}
---
+++ crates/ui-components/src/users.rs
@@ -0,0 +1,61 @@
+use crate::layout::Layout;
+use db::User;
+use dioxus::prelude::{*, dioxus_elements::tbody};
+
+struct Props {
+    users: Vec<User>,
+}
+
+// Vec<User> を取得し、HTML テーブルを作成します。
+pub fn users(users: Vec<User>) -> String {
+
+    // rsx を作成するための内部関数
+    fn app(cx: Scope<Props>) -> Element {
+        cx.render(rsx!(
+            Layout {
+                title: "Users Table",
+                table {
+                    thead {
+                        tr {
+                            th { "ID" }
+                            th { "Email" }
+                        }
+                    }
+                    tbody {
+                        cx.props.users.iter().map(|user| rsx!(
+                            tr {
+                                td {
+                                    strong { "{user.id}" }
+                                }
+                                td {
+                                    "{user.email}"
+                                }
+                            }
+                        ))
+                    }
+                }
+                form {
+                    action: "/sign_up",
+                    method: "POST",
+                    label {r#for: "user_email", "Email:" }
+                    input {
+                        id: "user_email",
+                        name: "user[email]",
+                        r#type: "email",
+                        required: "true",
+                    }
+                    button { "Submit" }
+                }
+            }
+
+        ))
+    }
+
+    // コンポーネントを構築し、それを文字列にレンダリングします。
+    let mut app =  VirtualDom::new_with_props(app, Props {
+        users
+    },
+);
+    let _ = app.rebuild();
+    dioxus::ssr::render_vdom(&app)
+}
